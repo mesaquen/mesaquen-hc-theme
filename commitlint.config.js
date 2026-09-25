32f8be24e664f78db776b2bcc52060e01a1da9a5
Mesaque Francisco <mesaquen@users.noreply.github.com>
2026-09-25 17:19:16 +0100

    chore(CHANGELOG): adding conventional commit support

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+const Configuration = {
+  extends: ["@commitlint/config-conventional"],
+  /**
+     rules: {
+        'type-enum': [
+        2,
+        'always', 
+            ['feat', 'fix', 'refactor', 'revert', 'style', 'test', 'docs', 'chore']
+        ]
+     }
+     */
+};
+
+export default Configuration;
