3ef37fe80e912f13364f92af268c1802661a406f
Mesaque Francisco <mesaquen@users.noreply.github.com>
2026-09-25 17:53:12 +0100

    fix(lint-staged): wrong braces matcher

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,5 @@
 const Configuration = {
   extends: ["@commitlint/config-conventional"],
-  /**
-     rules: {
-        'type-enum': [
-        2,
-        'always', 
-            ['feat', 'fix', 'refactor', 'revert', 'style', 'test', 'docs', 'chore']
-        ]
-     }
-     */
 };
 
 export default Configuration;
